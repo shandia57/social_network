784412e35e9f7e4e35ec37967a5bae541e9f8be9
shandia57 <shandia57bim@gmail.com>
2022-04-26 22:01:53 +0200

    Create first pages

---
+++ frontend/src/components/layout/Empty.jsx
@@ -0,0 +1,11 @@
+import { Outlet, Link } from "react-router-dom";
+
+const Empty = () => {
+    return (
+        <>
+            <Outlet />
+        </>
+    )
+};
+
+export default Empty;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,15 +10,20 @@ import './index.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Pages
-import App from './pages/App';
+import Home from './pages/home/Home';
+import Empty from './components/layout/Empty';
+import Signin from './pages/auth/signin/Signin';
+import Signup from './pages/auth/signup/Signup';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<App />} />
+        <Route path="/" element={<Empty />}>
+          <Route index element={<Signin />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="home" element={<Home />} />
         </Route>
         <Route path="">
 
